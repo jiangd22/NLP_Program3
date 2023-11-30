a5688875a6bc836b1ca919f99bd64dd3200425fd
RebeccaEKuhlman <rkuhlman@smu.edu>
2023-11-30 12:32:31 -0600

    Edited txt, added lstm file

---
+++ cs5322f23.py
@@ -0,0 +1,13 @@
+
+## where list is a list of string, each string is a sentence that contain the word “rubbish”.
+# You should output a list of numbers, each number corresponds to the sense of the word
+# “rubbish” for each sentence (either 1 or 2). Notice that you are responsible for the number
+# to match with the corresponding sentence, and each number is either 1 or 2 (NOT 0 or 1)
+def WSD_Test_Rubbish(list):
+    return
+
+def WSD_Test_Conviction(list):
+    return
+
+def WSD_Test_Thorn(list):
+    return
